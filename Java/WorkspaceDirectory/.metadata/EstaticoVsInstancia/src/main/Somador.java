abb63f2dc777992ae1541dfc02ab02ede3e1e862
Tauan Oliveira da Silva <tauan.silva@betha.com.br>
2021-04-23 11:52:57 -0300

    l

---
+++ Java/WorkspaceDirectory/.metadata/EstaticoVsInstancia/src/main/Somador.java
@@ -0,0 +1,15 @@
+package main;
+public class Somador{
+	String nome;
+	int ValorInstancia = 0;
+	static int ValorIstatico = 0;
+	
+	void Somar(){
+		ValorInstancia ++;
+		ValorIstatico ++;
+	}
+	
+	void imprimir() {
+		System.out.println("O somar: " +nome+ " instancia="+ValorInstancia+ " Istatico="+ValorIstatico);
+	}
+}
