64b68d574d61b9180cc274a99bd37cb6ebd0650c
Tauan Oliveira da Silva <tauan.silva@betha.com.br>
2021-04-27 11:56:37 -0300

    ExperienciaAcesso Commit

--- Java/WorkspaceDirectory/ExperienciaAcesso/src/outro/sub.java
+++ Java/WorkspaceDirectory/ExperienciaAcesso/src/outro/sub.java
@@ -1,5 +1,13 @@
 package outro;
 
-public class sub {
+import pacote.Referencia;
 
+public class sub extends Referencia{
+
+	public void testeacesso() {
+		//modificadorDefault = 0;
+		modificadorPublico = 0;
+		//modificadorPrivate = 0;
+		modificadorProtected = 0;
+	}
 }
--- Java/WorkspaceDirectory/ExperienciaAcesso/src/pacote/outroMesmoPacote.java
+++ Java/WorkspaceDirectory/ExperienciaAcesso/src/pacote/outroMesmoPacote.java
@@ -1,5 +1,12 @@
 package pacote;
 
 public class outroMesmoPacote {
-
+	
+	public void testeacesso() {
+		Referencia r = new Referencia();
+		r.modificadorDefault = 1;
+		r.modificadorPublico = 1;
+		//r.modificadorPrivate = 1;
+		r.modificadorProtected = 1;
+	}
 }
