06e8a17afa75c9480385b44b6d56d694501fe148
Tauan Oliveira da Silva <tauan.silva@betha.com.br>
2021-05-06 12:03:08 -0300

    q

--- Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/CursoMcApplication.java
+++ Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/CursoMcApplication.java
@@ -1,13 +1,29 @@
 package com.TauanOliveira.cursoMC;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.TauanOliveira.cursoMC.domain.Categoria;
+import com.TauanOliveira.cursoMC.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritótio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
