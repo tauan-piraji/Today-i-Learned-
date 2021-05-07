9553d60c82bcf5c938f8aa9b692fc9877c01613d
Tauan Oliveira da Silva <tauan.silva@betha.com.br>
2021-05-07 11:08:32 -0300

    Produto e associação

--- Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/CursoMcApplication.java
+++ Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/CursoMcApplication.java
@@ -1,18 +1,24 @@
 package com.TauanOliveira.cursoMC;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.TauanOliveira.cursoMC.domain.Categoria;
+import com.TauanOliveira.cursoMC.domain.Produto;
 import com.TauanOliveira.cursoMC.repositories.CategoriaRepository;
+import com.TauanOliveira.cursoMC.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -23,7 +29,19 @@ public class CursoMcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritótio");
 		
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora", 800.00);
+		Produto p3 = new Produto(null,"Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/repositories/ProdutoRepository.java
+++ Projeto full stack/cursoMC/src/main/java/com/TauanOliveira/cursoMC/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.TauanOliveira.cursoMC.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.TauanOliveira.cursoMC.domain.Categoria;
+import com.TauanOliveira.cursoMC.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 }
